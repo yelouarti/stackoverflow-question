8b831a89e0777655b15711fdf967a2b67684d187
Younes El Ouarti <y.elouarti@outlook.com>
2024-12-04 15:37:11 +0100

    rm copyright statement

--- src/main/java/elouarti/younes/stackoverflowquestion/pojo/Person.java
+++ src/main/java/elouarti/younes/stackoverflowquestion/pojo/Person.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c)  2024 kamillion-suite contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @since 03.12.2024
- */
-/*
- * Copyright (c) 2024 kamillion contributors
- *
- * This work is licensed under the GNU General Public License (GPL).
- *
- * @since 14.10.2024
- */
-
 package elouarti.younes.stackoverflowquestion.pojo;
 
 import lombok.AllArgsConstructor;
